0b2caa944a9e5b2f0d4a40018a1265b6922d78b5
AlexMachariaMaranga <amaranga22@gmail.com>
2023-01-18 10:55:19 +0300

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,10 @@
 import React from "react";
+import {v4 as uuid} from "uuid"
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-  
-  const allLinks = links.map((link, index) => {
-    return <li key={index}>{link}</li>
-  })
-  return <nav>{allLinks }</nav>;
+
+  return <nav>{links.map((link)=> <a key={uuid()}href={`#${link}`}>{link}</a> )}</nav>;
 }
 
 export default NavBar;
